ca0913006988d9c3213937a54a1c7df7d8931e9d
Alice Veselá <alice.hladikova@gmail.com>
2024-04-28 19:01:28 +0200

    pridani API protihrace

--- hra2.js
+++ hra2.js
@@ -2,8 +2,7 @@ import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 
 const gameArray = Array.from(document.querySelectorAll('.field__button'));
 let currentPlayer = 'circle';
-
-const selectSquare = (evt) => {
+const selectSquare = async (evt) => {
   evt.target.disabled = true;
   if (currentPlayer === 'circle') {
     evt.target.classList.add('field__button--circle');
@@ -31,16 +30,34 @@ const selectSquare = (evt) => {
       alert(`Vyhrál hráč se symbolem ${winner}.`);
       location.reload();
     }, 500);
+  } else if (currentPlayer === 'cross') {
+    const response = await fetch(
+      'https://piskvorky.czechitas-podklady.cz/api/suggest-next-move',
+      {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          board: gameArrayFilled,
+          player: 'x',
+        }),
+      },
+    );
+    const data = await response.json();
+    const { x, y } = data.position;
+    const field = gameArray[x + y * 10];
+    field.click();
   }
-
-  const repeat = document.querySelector('.icon--repeat');
-  repeat.addEventListener('click', (event) => {
-    const confirmation = confirm('Opravdu chcete obnovit hru?');
-    if (!confirmation) {
-      event.preventDefault();
-    }
-  });
 };
+
+const repeat = document.querySelector('.icon--repeat');
+repeat.addEventListener('click', (event) => {
+  const confirmation = confirm('Opravdu chcete obnovit hru?');
+  if (!confirmation) {
+    event.preventDefault();
+  }
+});
 
 gameArray.forEach((field) => {
   field.addEventListener('click', selectSquare);
